8c86593b1e6cc136312bf587081fa713c05d8aa1
anil-s-yadav <paytmking15@gmail.com>
2025-06-11 11:18:19 +0530

    amenities basics done.

--- lib/amenities/user_amenities_page.dart
+++ lib/amenities/user_amenities_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:society_gate/amenities/myamenity_details.dart';
 import 'package:society_gate/api/api_repository.dart';
 import 'package:society_gate/constents/local_storage.dart';
 import 'package:society_gate/models/get_user_purchase_amenities_model.dart';
+
+import 'amenities_images.dart';
 
 class UserAmenitiesPage extends StatefulWidget {
   final List<String> amenities;
@@ -24,9 +27,12 @@ class _UserAmenitiesPageState extends State<UserAmenitiesPage> {
   _getData() async {
     final data = LocalStoragePref.instance!.getLoginModel();
     ApiRepository apiRepository = ApiRepository();
-    getUserPurchaseAmenitiesData =
+    final amenitiesData =
         await apiRepository.getFamilyMemGetUserPurchaseAmenities(
             data!.user!.societyId.toString(), data.user?.userId.toString());
+    setState(() {
+      getUserPurchaseAmenitiesData = amenitiesData;
+    });
   }
 
   @override
@@ -35,80 +41,90 @@ class _UserAmenitiesPageState extends State<UserAmenitiesPage> {
       appBar: AppBar(
         title: const Text("My Amenities"),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.pinkAccent,
+        foregroundColor: Colors.white,
       ),
       body: getUserPurchaseAmenitiesData?.data == null
           ? const Center(
-              child: Text(
-                "No amenities added yet.",
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-            )
+              child: CircularProgressIndicator(
+              color: Colors.pinkAccent,
+            ))
           : ListView.builder(
               padding: const EdgeInsets.all(16),
               itemCount: getUserPurchaseAmenitiesData?.data?.length ?? 0,
               itemBuilder: (context, index) {
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  elevation: 4,
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Amenity Image
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.asset(
-                            'lib/assets/swimming-pool.png', // Replace with your image URL
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyAmenityDetails(
+                              data:
+                                  getUserPurchaseAmenitiesData!.data![index]))),
+                  child: Card(
+                    // color: Colors.pink.shade50,
+                    surfaceTintColor: Colors.pink.shade50,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Amenity Image
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              getAmenityImage(getUserPurchaseAmenitiesData!
+                                      .data![index].amenity!.name ??
+                                  ""), // Replace with your image URL
+                              width: 70,
+                              height: 70,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        const SizedBox(width: 12),
-                        // Text Info
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                widget.amenities[index], // Amenity name
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                          const SizedBox(width: 12),
+                          // Text Info
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  getUserPurchaseAmenitiesData!
+                                          .data![index].amenity!.name ??
+                                      "", // Amenity name
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 6),
-                              Text(
-                                getUserPurchaseAmenitiesData!
-                                        .data?[index].duration
-                                        .toString() ??
-                                    "",
-                                style: const TextStyle(
-                                    fontSize: 14, color: Colors.grey),
-                              ),
-                              const SizedBox(height: 4),
-                              Text(
-                                getUserPurchaseAmenitiesData!
-                                        .data?[index].amount
-                                        .toString() ??
-                                    "",
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black87,
-                                  fontWeight: FontWeight.w600,
+                                const SizedBox(height: 6),
+                                Text(
+                                  "• End at:  ${getUserPurchaseAmenitiesData!.data?[index].endTime ?? "".toString()} ",
+                                  style: TextStyle(
+                                      fontSize: 14, color: Colors.red.shade300),
                                 ),
-                              ),
-                            ],
+                                const SizedBox(height: 4),
+                                Text(
+                                  getUserPurchaseAmenitiesData!
+                                          .data?[index].duration
+                                          .toString() ??
+                                      "",
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        const Icon(Icons.arrow_forward_ios,
-                            size: 16, color: Colors.grey),
-                      ],
+                          const Icon(Icons.arrow_forward_ios,
+                              size: 16, color: Colors.grey),
+                        ],
+                      ),
                     ),
                   ),
                 );
